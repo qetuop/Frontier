9863eadc5271919e11eceb09f1abd7d516bf2039
qetuop <newsgrpacc@yahoo.com>
2017-05-28 18:44:59 -0400

    - initial commit

---
+++ src/frontier/Humanoid.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frontier;
+
+import frontier.AStar.Node;
+import java.util.LinkedList;
+import tiled.core.Tile;
+
+/**
+ *
+ * @author brian
+ */
+public class Humanoid extends Sprite {
+    LinkedList<AStar.Node> path;
+    
+    public Humanoid(Tile tile, int startX, int startY) {
+        super(tile, startX, startY);
+    }
+    
+    public void moveChar() {
+        if ( path.size() == 0 ) return;
+        Node node = path.pop();
+        if ( node != null ) {
+            positionX = node.x;
+            positionY = node.y;
+        }
+    }
+    
+}
---
+++ src/frontier/Utils.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frontier;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javafx.scene.image.Image;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author brian
+ */
+public class Utils {
+    public static Image createImage(java.awt.Image image) throws IOException {
+        if (!(image instanceof RenderedImage)) {
+            BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),
+                    image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+            Graphics g = bufferedImage.createGraphics();
+            g.drawImage(image, 0, 0, null);
+            g.dispose();
+
+            image = bufferedImage;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ImageIO.write((RenderedImage) image, "png", out);
+        out.flush();
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        return new javafx.scene.image.Image(in);
+    } // createImage
+}
